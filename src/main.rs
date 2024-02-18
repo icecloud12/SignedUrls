8325fd45262d51e2c11af69a84b8be87ba52d9fb
icecloud12 <johnsonjaurigue2@gmail.com>
2024-02-18 22:17:18 +0800

    chore: base file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::{Html, IntoResponse}, routing::get, Json, Router};
+use std::format;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route("/hello", get(|| async {
+        Html("Hello World!")
+    }));
+    let address: &str="127.0.0.1";
+    let port:i32 = 3001;
+    let listener = tokio::net::TcpListener::bind(format!("{address}{port}",address = address, port = port)).await.unwrap();
+    println!("---->>> Listening on 0.0.0.0:3000\n");
+    axum::serve(listener, routes_hello).await.unwrap();
 }
