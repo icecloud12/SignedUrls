cd0c66ff635cba672d4d32ce55e4825457bb2ac4
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-02-20 16:56:53 +0800

    feat: added router helper

--- src/util/HttpRequestHelper.rs
+++ src/util/HttpRequestHelper.rs
@@ -1,22 +1,3 @@
-// use std::net::TcpStream;
-// struct Path {
-//     domain: &str,
-//     port: &str,
-//     path: &str,
-//     query: &str,
-//     fragments: &str,
-
-// }
-
-use std::{
-    net::TcpStream,
-    io::{
-        Result,
-        prelude::*
-    },
-    str::*, clone
-};
-
 pub struct HTTPRequest {
     pub protocol: String,
     pub path: String,
@@ -33,7 +14,70 @@ pub  fn wrap (line:String) -> HTTPRequest{
     }
 }
 
-
-enum Method {
+pub enum ProtocolCollection {
     GET,
+    POST,
+    PUT,
+    PATCH
 }
+
+impl ToString for ProtocolCollection{
+    fn to_string(&self) -> String {
+        match self {
+            Self::GET => String::from("GET"),
+            Self::POST => String::from("POST"),
+            Self::PUT => String::from("PUT"),
+            Self::PATCH => String::from("PATCH"),
+        }
+    }
+}
+pub struct Route
+{
+    pub protocol: String,
+    pub route: String,
+    pub handler: Box<dyn FnOnce()>
+}
+pub struct Router
+{
+    pub routes: Vec<Route>,
+}
+impl Router {
+    pub fn new() -> Self{
+        Self {
+            routes: vec![]
+        }
+    }
+
+    fn add_route(&mut self, path:&str, protocol:String, handler: impl FnOnce() + 'static)
+    {
+        let route:Route = Route {
+            protocol,
+            route: path.to_string(),
+            handler: Box::new(handler)
+        };
+        self.routes.push(route);
+    }
+
+    pub fn get(&mut self, path:&str, handler:impl FnOnce() + 'static)
+    {
+        self.add_route(path, ProtocolCollection::GET.to_string(),handler);
+    }
+    
+    pub fn post(&mut self, path:&str, handler: impl FnOnce() +'static)
+    {
+        self.add_route(path, ProtocolCollection::POST.to_string(), handler);
+    }
+    
+    pub fn put(&mut self, path:&str, handler: impl FnOnce() +'static)
+    {
+        self.add_route(path, ProtocolCollection::PUT.to_string(), handler);
+    }
+
+    pub fn patch(&mut self, path:&str, handler: impl FnOnce() +'static)
+    {
+        self.add_route(path, ProtocolCollection::PATCH.to_string(), handler);
+    }
+    
+}
+
+
