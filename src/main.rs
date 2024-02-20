017332eebc375f7ee5cd8a9bf62741c1f7945dfe
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-02-20 16:58:05 +0800

    refactor: minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod util;
 
-use axum::{response::{Html}, routing::get, Router};
-use mongodb::{options::{ClientOptions, ResolverConfig}, Client, Collection};
+use axum::{response::{Html}, routing::get};
+//use mongodb::{options::{ClientOptions, ResolverConfig}, Client, Collection};
 use util::collection::MongoDbCollection::{self, SampleCollection};
 use std::{
     io::{
@@ -15,18 +15,28 @@ use std::{
 use axum_tut::ThreadPool;
 use util::HttpRequestHelper::{
     *,
-    HTTPRequest, self
+    HTTPRequest, 
+    Router,
+    self
 };
+
 const IP_ADDRESS:&str = "127.0.0.1";
 const PORT:&str = "7878";
+
 fn main(){
-    let sampleCollection = MongoDbCollection::SampleCollection.to_string();
+    
+    let mut router: Router = Router::new();
+    router.get("/", ||{
+        
+    });
+    listen(router);
+}
+fn listen(router: Router){
     let address: String = format!("{IP_ADDRESS}:{PORT}");
     let listener: Result<TcpListener> = TcpListener::bind(address);
     let pool = ThreadPool::new(4);
     match listener {
         Ok(listener) => {
-            println!("OK");
             for l in listener.incoming(){
                 pool.execute(||{
                     let mut l_stream: TcpStream = l.unwrap();
@@ -47,25 +57,26 @@ fn handle_connection(mut stream:TcpStream){
     let mut lines = buffer.lines();
 
     let http_request: HTTPRequest = HttpRequestHelper::wrap(lines.next().unwrap().unwrap());
+    let protocol_collection:Vec<&str> = vec!["GET", "POST", "PUT", "PATCH"];
 
-    let protocol_collection:Vec<&str> = vec!["GET"];
     let protocol = http_request.protocol;
-    let file_path: &str;
+    
     let status_code: &str;
     let status_message: &str;
     if(protocol_collection.contains(&protocol.as_ref())){
-        file_path = "index.html";
+        //file_path = "index.html";
         status_code = "200";
 
         status_message = "OK";
     }
     else {
-        file_path = "error.html";
+        // /file_path = "error.html";
         status_code = "404";
         status_message = "Resource Not Found"
     }
-    let file = fs::read_to_string(file_path).unwrap();
-    let response: String = format!("HTTP/1.1 {} {} \r\nContent-Length: {}\r\n\r\n{}",status_code, status_message, file.len(), file);
+    //let file = fs::read_to_string(file_path).unwrap();
+    let content = "Hello world";
+    let response: String = format!("HTTP/1.1 {} {} \r\nContent-Length: {}\r\n\r\n{}",status_code, status_message, content.len(), content);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
     
