f48bb8f7c40f60e49214c8a3e4b7e7aacf30f8e9
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-02-26 16:35:53 +0800

    feat: create project now searches if record exists

--- src/actions/Project.rs
+++ src/actions/Project.rs
@@ -1,15 +1,30 @@
-use mongodb::{results::InsertOneResult, Collection, Database};
-use crate::network;
+use mongodb::{results::InsertOneResult, Collection, Database, bson::doc};
+use crate::network::{Db_Connection::DATABASE, DB_Collection};
 use crate::models::Project::ProjectModel;
 
-pub fn Collection(database: &Database) -> Collection<ProjectModel>{
-    let collection = database.collection::<ProjectModel>(network::DB_Collection::PROJECT.to_string().as_str());
-    return collection;
-}
-pub async fn Create(database: &Database, project_name:String) -> Result<InsertOneResult, mongodb::error::Error>{
-    let collection = Collection(&database);
-    let doc: ProjectModel = ProjectModel{
-        name: project_name,
-    };
-    return collection.insert_one(doc, None).await; 
+pub async fn create_project( project_name:String) //-> Result<InsertOneResult, mongodb::error::Error>
+{
+    let db= DATABASE.get().unwrap();
+
+    //check if exists
+    let if_exist_result = db.collection::<ProjectModel>(DB_Collection::PROJECT.to_string().as_str()).find_one(doc! {
+        "name": project_name.as_str()
+    }, None).await;
+    
+    match if_exist_result {
+        Ok(o_project_model) => {
+            //it exsits
+        },
+        Err(error) => {
+            //does not exist then insert
+            let doc= ProjectModel{
+                name: project_name
+            };
+            db.collection::<ProjectModel>(DB_Collection::PROJECT.to_string().as_str()).insert_one(doc, None);
+
+        }
+    }
+    //create directory
+
+    
 }
