0f4b3401236b7bf0bda0667b8148cd5cf1d21cbe
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-02-28 16:59:15 +0800

    chore: minor changes

--- src/handlers/project_handler.rs
+++ src/handlers/project_handler.rs
@@ -1,25 +1,26 @@
 use axum::response::{IntoResponse, Response};
 use hyper::StatusCode;
 
+use mongodb::Database;
 use serde::Deserialize;
 use axum::{extract::Json};
 use serde_json::json;
 
 use crate::actions::project;
-use crate::network::Connect;
+use crate::network;
 #[derive(Deserialize)]
 pub struct CreateProjectPostRequest {
     pub name: String,
 }
 
 pub async fn create_project(Json(post_request):Json<CreateProjectPostRequest>) -> impl IntoResponse{
-    let db = Connect().await;
+    let db = network::Db_Connection::DATABASE.get().unwrap();
     //check if exist
     let create_request = project::insert_project_if_exists( &post_request.name).await;
     match create_request {
         Some(request) => {
             //then create directory
-            project::create_project_directory(&request);
+            project::create_project_directory(&request).await;
             let j = json!({"data":{"id":request, "project_name": post_request.name.as_str()}, "message":"success"});
 
             return (StatusCode::CREATED, Json(j)).into_response();
@@ -28,9 +29,6 @@ pub async fn create_project(Json(post_request):Json<CreateProjectPostRequest>) -
             let j = json!({"data":"", "message":"Project already exists"});
             return (StatusCode::CONFLICT,Json(j)).into_response();
         }
-    };
-    
-    
-
+    };   
 }
 
