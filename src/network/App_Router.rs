509ff6cae29f1c62234c1377bf71ca6f0207327b
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-02-28 16:57:24 +0800

    chore: temp path for creating request

--- src/network/App_Router.rs
+++ src/network/App_Router.rs
@@ -1,10 +1,13 @@
 use axum::{Router, routing::{get, post}};
-use crate::handlers::project_handler::{
-    create_project
+use crate::handlers::{
+    project_handler,
+    //request_handler
 };
 pub async fn Router()->axum::Router {
-    let router = Router::new().
-        route("/project/create", post(create_project));
+    let router = Router::new()
+        .route("/project/create", post(project_handler::create_project))
+        //.route("/request/create", post(request_handler::create_request))
+        ;
     return router;
 
 }
