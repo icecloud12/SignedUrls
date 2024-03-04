a23f79a704164b3e617536d20ee2dd3ddef9bd8f
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-03-04 16:36:31 +0800

    chore: wip file saving;

--- src/signed_url/handlers.rs
+++ src/signed_url/handlers.rs
@@ -1,21 +1,24 @@
 use axum::{
-    response:: IntoResponse,
     extract::{
-        Path,
-        Json,
-    }
+        Json, Path
+    }, response:: IntoResponse,
+    extract::Multipart
 };
 use hyper::StatusCode;
 use serde_json::json;
-use super::models::ExtractSignedUrlParametersRequest;
 
 pub async fn process_signed_url_request(
     Path(params):Path<Vec<(String,String)>>,
-    Json(body): Json<ExtractSignedUrlParametersRequest>
+    mut multipart: Multipart,
 )-> impl IntoResponse{
     
+    while let Some(mut part) = multipart.next_field().await.unwrap() {
+        println!("{:#?}", part);
+        let name = part.name().unwrap().to_string();
+        let data = part.bytes().await.unwrap();
+        
+    }
     return (StatusCode::OK, Json(json!({"data":{
         "params":params,
-        "body": body
     }})));
 }
