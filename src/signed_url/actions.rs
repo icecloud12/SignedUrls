a8cd134c899da2adc251a78b1e9a893dbb48e9ed
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-03-06 16:29:52 +0800

    refactor

--- src/signed_url/actions.rs
+++ src/signed_url/actions.rs
@@ -1,17 +1,18 @@
-use axum::{body::Bytes, Error};
+use axum::{body::Bytes, extract::{multipart, Multipart}, response::IntoResponse, Error, Json};
+use hyper::StatusCode;
+use mongodb::{bson::{doc, oid::ObjectId}, Database};
+use serde_json::{from_str, json};
 use sha3::{Digest, Sha3_256};
 use rand::{self, Rng};
 use std::{
-    path::Path,
-    ffi::OsStr,
-    time::{
+    num::ParseIntError, path::Path, str::FromStr, time::{
         SystemTime, UNIX_EPOCH
-    },
+    }
 };
-
+use crate::{network::{db_connection::DATABASE, DbCollection}, project::models::ProjectDocument};
 
 use tokio::{fs::File, io::AsyncWriteExt};
-use crate::project;
+use crate::{request::model::RequestDocument};
 pub enum ActionTypes {
     UPLOAD,
 }
@@ -68,18 +69,102 @@ pub fn hash_parameters(
     return hashed_signature_base_64;
 }
 
+pub struct ValidateSignedUrlResultUploadFiles {
+    bytes: Vec<Bytes>,
+    names: Vec<String>,
+    options: Vec<String>
+
+}
+pub async fn validate_signed_url( 
+    params: Vec<(String,String)>,
+    mut multipart: Multipart
+) -> Result<ValidateSignedUrlResultUploadFiles, String> {
+    let collected_params = params.iter().map(|param| param.1.clone()).collect::<Vec<String>>();
+    let (
+        request_id,
+        permission,
+        created,
+        expiration,
+        nonce,
+        signature
+    ) = (
+        collected_params[0].to_owned(),
+        collected_params[1].to_owned(),
+        collected_params[2].to_owned(),
+        collected_params[3].to_owned(),
+        collected_params[4].to_owned(),
+        collected_params[5].to_owned()
+    );
+    let created_time:Result<u64, ParseIntError> = created.parse();
+    let expiration_time:Result<u64, ParseIntError> = expiration.parse();
+    if created_time.is_ok() && expiration_time.is_ok() {
+        let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+        if current_time >= created_time.unwrap() && current_time <= expiration_time.unwrap() {
+        //fetch project_id from request id
+            let db: &Database = DATABASE.get().unwrap();
+            let request_entry = db.collection::<RequestDocument>(DbCollection::REQUEST.to_string().as_str()).find_one(doc!{"_id":ObjectId::from_str(&request_id).unwrap()}, None).await;
+
+            if request_entry.is_ok(){
+                let project_name = request_entry.unwrap().unwrap().project_name;
+                let project_entry = db.collection::<ProjectDocument>(DbCollection::PROJECT.to_string().as_str()).find_one(doc! {"name": project_name}, None).await.unwrap().unwrap();
+
+                let replicated_hash = hash_parameters(&project_entry._id.to_string(), &from_str::<u64>(&created).unwrap(), &from_str::<u64>(&expiration).unwrap(), &permission, &from_str::<u64>(&nonce).unwrap());
+                if(replicated_hash == signature){
+                    //if valid extract the necessary information
+                    let mut file_bytes:Vec<Bytes>= vec![];
+                    let mut file_options:Vec<String>=vec![];
+                    let mut file_names:Vec<String> = vec![];
+                    println!("hashed signature valid");
+                    while let Some(mut part) = multipart.next_field().await.unwrap() {
+                        
+                        if part.name().unwrap_or_else(|| "") == "files" {
+                            
+                            let file_name = part.file_name().unwrap().to_string();
+                            let data: Bytes = part.bytes().await.unwrap();
+                            //assume content type containing parts are files
+                            file_bytes.push(data);
+                            file_names.push(file_name);
+
+                        }else if part.name().unwrap_or_else(|| "") == "fileOptions"{
+                            let data = part.bytes().await.unwrap();
+                            let val:String = String::from_utf8(data.to_vec()).unwrap();
+                            file_options.push(val);
+                        }
+
+                    }
+                    return Ok(ValidateSignedUrlResultUploadFiles {
+                        bytes: file_bytes,
+                        names: file_names,
+                        options: file_options
+                    });
+                }else{
+                    println!("hashed signature invalid");
+                    return Err("hashed signature invalid".to_string());
+                }
+            
+
+            }else{
+                println!("hashed signature invalid");
+                return Err("Hashed Signature not authorized".to_string());
+            }
+
+        }
+    }
+    println!("hashed signature invalid");
+    return Err("Hashed Signature not authorized".to_string());
+}
+
 pub async fn save_files_to_directory(
-    file_names:Vec<String>,
-    file_bytes: Vec<Bytes>,
-    file_options: Vec<String>,
+
+    result: ValidateSignedUrlResultUploadFiles
 
 )-> Result<(), bool>{
     //the request itself should have a file path to go to
-    for (index, element) in file_bytes.iter().enumerate(){
+    for (index, element) in result.bytes.iter().enumerate(){
         let file_name = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis().to_string();
         
-        println!("fileNames: {}", file_names[index].as_str());
-        let splits = file_names[index].as_str().split(".");
+        println!("fileNames: {}", result.names[index].as_str());
+        let splits = result.names[index].as_str().split(".");
         let file_extention = splits.last().unwrap();
         println!("{}", file_extention);
         let mut file:File = File::create(format!("./data/{}.{}",file_name,file_extention)).await.unwrap();
@@ -87,3 +172,4 @@ pub async fn save_files_to_directory(
     }
     Ok(())
 }
+
--- src/signed_url/handlers.rs
+++ src/signed_url/handlers.rs
@@ -1,95 +1,27 @@
-use std::{num::ParseIntError, str::FromStr, time::{SystemTime, UNIX_EPOCH}};
+
 
 use axum::{
-    body::Bytes, extract::{
+    extract::{
         Json, Multipart, Path
-    }, http::request, response:: IntoResponse
+    }, response:: IntoResponse
 };
-use mongodb::{bson::{doc, oid::ObjectId}, Database};
-use crate::{network, project::{self, models::ProjectDocument}, request::model::RequestDocument, signed_url::actions::save_files_to_directory};
+use crate::signed_url::actions::{save_files_to_directory, validate_signed_url};
 
-use super::actions::hash_parameters;
 use hyper::StatusCode;
-use serde_json::{from_str, json};
-use network::db_connection::DATABASE;
-use network::DbCollection;
-use crate::util::collection::MongoDbCollection;
+use serde_json::json;
+
 pub async fn process_signed_url_request(
     Path(params):Path<Vec<(String,String)>>,
     mut multipart: Multipart,
 )-> impl IntoResponse{
     //validate the url
-    let collected_params = params.iter().map(|param| param.1.clone()).collect::<Vec<String>>();
-    let (
-        request_id,
-        permission,
-        created,
-        expiration,
-        nonce,
-        signature
-    ) = (
-        collected_params[0].to_owned(),
-        collected_params[1].to_owned(),
-        collected_params[2].to_owned(),
-        collected_params[3].to_owned(),
-        collected_params[4].to_owned(),
-        collected_params[5].to_owned()
-    );
-    let created_time:Result<u64, ParseIntError> = created.parse();
-    let expiration_time:Result<u64, ParseIntError> = expiration.parse();
-    if created_time.is_ok() && expiration_time.is_ok() {
-        let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-        if current_time >= created_time.unwrap() && current_time <= expiration_time.unwrap() {
-        //fetch project_id from request id
-            let db: &Database = DATABASE.get().unwrap();
-            let request_entry = db.collection::<RequestDocument>(DbCollection::REQUEST.to_string().as_str()).find_one(doc!{"_id":ObjectId::from_str(&request_id).unwrap()}, None).await;
-
-            if request_entry.is_ok(){
-                let project_name = request_entry.unwrap().unwrap().project_name;
-                let project_entry = db.collection::<ProjectDocument>(DbCollection::PROJECT.to_string().as_str()).find_one(doc! {"name": project_name}, None).await.unwrap().unwrap();
-
-                let replicated_hash = hash_parameters(&project_entry._id.to_string(), &from_str::<u64>(&created).unwrap(), &from_str::<u64>(&expiration).unwrap(), &permission, &from_str::<u64>(&nonce).unwrap());
-                if(replicated_hash == signature){
-                    //if valid extract the necessary information
-                    let mut file_bytes:Vec<Bytes>= vec![];
-                    let mut file_options:Vec<String>=vec![];
-                    let mut file_names:Vec<String> = vec![];
-                    println!("hashed signature valid");
-                    while let Some(mut part) = multipart.next_field().await.unwrap() {
-                        
-                        if part.name().unwrap_or_else(|| "") == "files" {
-                            
-                            let file_name = part.file_name().unwrap().to_string();
-                            let data: Bytes = part.bytes().await.unwrap();
-                            //assume content type containing parts are files
-                            file_bytes.push(data);
-                            file_names.push(file_name);
-
-                        }else if part.name().unwrap_or_else(|| "") == "fileOptions"{
-                            let data = part.bytes().await.unwrap();
-                            let val:String = String::from_utf8(data.to_vec()).unwrap();
-                            file_options.push(val);
-                        }
-
-                    }
-                    let _save = save_files_to_directory(file_names, file_bytes, file_options).await;
-                    return (StatusCode::OK, Json(json!({"data":{
-                        "params":params,
-                    }})));
-                }else{
-                    println!("hashed signature invalid");
-                    return (StatusCode::BAD_REQUEST, Json(json!({"data":"Hashed Signature not authorized"})));
-                }
-            
-
-            }else{
-                println!("hashed signature invalid");
-                return (StatusCode::BAD_REQUEST, Json(json!({"data":"Hashed Signature not authorized"})));
-            }
-
+    match validate_signed_url(params, multipart).await {
+        Ok(validatedResult) =>{
+            save_files_to_directory(validatedResult);
+            return (StatusCode::OK,Json(json!({"data":"Operation Successful"})));
         }
-    }
-    println!("request expired");
-    return (StatusCode::BAD_REQUEST, Json(json!({"data":"Request Expired"})));
+        Err(e) => {return (StatusCode::BAD_REQUEST, Json(json!({"data":"Request Expired"})))}
+    };
+    
     
 }
