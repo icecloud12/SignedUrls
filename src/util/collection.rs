2b22aca4a5410d4564fb2a4adca991bdc2928bfe
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-03-07 15:49:46 +0800

    chore: clean-up

--- src/util/collection.rs
+++ src/util/collection.rs
@@ -1,13 +1,13 @@
-pub enum MongoDbCollection{
-    Project, // project name
-    Collection, // a folder inside the project
-}
+// pub enum MongoDbCollection{
+//     Project, // project name
+//     Collection, // a folder inside the project
+// }
 
-impl ToString for MongoDbCollection {
-    fn to_string(&self) -> String{
-        match self {
-            Self::Project => String::from("Project"),
-            Self::Collection => String::from("Collection")
-        }
-    }
-}
+// impl ToString for MongoDbCollection {
+//     fn to_string(&self) -> String{
+//         match self {
+//             Self::Project => String::from("Project"),
+//             Self::Collection => String::from("Collection")
+//         }
+//     }
+// }
