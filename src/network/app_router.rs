be212457937ad9b383bc86eee64afd1784138813
johnsonjaurigue <johnsonjaurigue2@gmail.com>
2024-03-07 15:48:39 +0800

    chore: minor rename

--- src/network/app_router.rs
+++ src/network/app_router.rs
@@ -1,14 +1,14 @@
 use axum::{Router, routing::post};
 use crate::project::handlers::create_project;
-use crate::request::handlers::create_request;
-use crate::signed_url::handlers::process_signed_url_request;
+use crate::request::handlers::create_upload_request;
+use crate::signed_url::handlers::process_signed_url_upload_request;
 
 pub async fn router()->axum::Router {
     let router = Router::new()
         .route("/project/create", post(create_project))
-        .route("/request/create", post(create_request))
+        .route("/request/create/upload", post(create_upload_request))
         //HANDLE THE SIGNED URL
-        .route("/id/:project_id/permission/:permission/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature", post(process_signed_url_request))
+        .route("/id/:project_id/permission/upload/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature", post(process_signed_url_upload_request))
         ;
     return router;
 
