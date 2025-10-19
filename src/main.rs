d51b8233a53e92235f83432f6c70017c5a63be26
Johnson <johnsonjaurigue2@gmail.com>
2025-10-19 16:59:37 +0800

    chore: added better logs

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,9 @@ async fn main(){
     let router = app_router::router().await;
     let address=std::env::var("ADDRESS").unwrap();
     let port = std::env::var("PORT").unwrap();
-    println!("{}:{}", address, port );
+
+    let subsciber = tracing_subscriber::fmt().compact().with_file(true).with_line_number(true).with_target(true).finish();
+    tracing::subscriber::set_global_default(subsciber).unwrap();
     
     match RustlsConfig::from_pem_file(
         PathBuf::from("./src/cert/")
@@ -29,17 +31,16 @@ async fn main(){
             .join("localhost.pem")
     ).await {
         Ok(config)=>{
-            println!("CERTS OK");
+            tracing::info!("TLS Certificates found");
             let ip: Vec<u8> = env::var("ADDRESS").unwrap().split(".").into_iter().map(|x| x.parse::<u8>().unwrap()).collect::<Vec<u8>>();
             let socket_address: [u8; 4] = [ip[0],ip[1],ip[2],ip[3]];
             let addr = SocketAddr::from((socket_address, env::var("PORT").unwrap().parse::<u16>().unwrap()));
             let addr_s = &addr.to_string();
-            println!("listening on {}", addr_s);
+            tracing::info!("listening on https://{addr_s}");
             axum_server::bind_rustls(addr,config).serve(router.into_make_service()).await.unwrap()        
         }
         Err(e)=>{ 
-            println!("CERTS NOT FOUND");
-            println!("error:{:#?}",e) 
+            tracing::error!("Missing certificaes");
         }
     }
     
