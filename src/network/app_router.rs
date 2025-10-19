fe62da27ecf0f54d7c20c506016301f48831ec70
Johnson <johnsonjaurigue2@gmail.com>
2025-10-19 12:48:10 +0800

    chore: added api versioning

--- src/network/app_router.rs
+++ src/network/app_router.rs
@@ -10,8 +10,7 @@ use crate::request::handlers::{
 };
 use crate::signed_url::handlers::{
     process_signed_url_view_request,
-    process_signed_url_upload_request,
-    direct_upload
+    process_signed_url_upload_request
 };
 
 pub async fn router()->axum::Router {
@@ -19,19 +18,38 @@ pub async fn router()->axum::Router {
 
     let router = Router::new()
         //create the API_Key
-        .route(format!("{}/project/create",prefix).as_str(), post(create_project))
-        //CREATE uplaod/view requests
-        .route(format!("{}/request/create/upload",prefix).as_str(), post(create_upload_request))
-        .route(format!("{}/request/create/view",prefix).as_str(), post(create_view_request))
+        .route(format!("{}/project/create",prefix).as_str(), post(create_project))//Legal Alias
+        .route(format!("{}/v1/project/create",prefix).as_str(), post(create_project))
+        .route(format!("{}/v2/project/create", prefix).as_str(), post(create_project))//Formalization
+
+        //CREATE view requests
+        .route(format!("{}/request/create/view",prefix).as_str(), post(create_view_request))//Legal Alias
+        .route(format!("{}/v1/request/create/view",prefix).as_str(), post(create_view_request))
+        .route(format!("{}/v2/request/create/view",prefix).as_str(), post(create_view_request))//Formalization
+        
+        //CREATE upload requests
+        .route(format!("{}/request/create/upload",prefix).as_str(), post(create_upload_request))//Legal Alias
+        .route(format!("{}/v1/request/create/upload",prefix).as_str(), post(create_upload_request))
+        .route(format!("{}/v2/request/create/upload",prefix).as_str(), post(create_upload_request))//Formalization
+
         //signed-url upload
-        .route(format!("{}/id/:request_id/permission/upload/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature",prefix).as_str(), post(process_signed_url_upload_request))
+        .route(format!("{}/id/:request_id/permission/upload/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature",prefix).as_str(), post(process_signed_url_upload_request))//Legal Alias
+        .route(format!("{}/v1/id/:request_id/permission/upload/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature",prefix).as_str(), post(process_signed_url_upload_request))
+        .route(format!("{}/v2/id/:request_id/permission/upload/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature",prefix).as_str(), post(process_signed_url_upload_request))//Formalization
+
         //signed-url-view
-        .route(format!("{}/id/:request_id/permission/view/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature/file/:file_id",prefix).as_str(),  get(process_signed_url_view_request))
+        .route(format!("{}/id/:request_id/permission/view/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature/file/:file_id",prefix).as_str(),  get(process_signed_url_view_request))//Legal Alias
+        .route(format!("{}/v1/id/:request_id/permission/view/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature/file/:file_id",prefix).as_str(),  get(process_signed_url_view_request))
+        .route(format!("{}/v2/id/:request_id/permission/view/created/:created/expiration/:expiration/nonce/:nonce/signature/:signature/file/:file_id",prefix).as_str(),  get(process_signed_url_view_request))//Formalization
+
         //public preview
-        .route(format!("{}/preview/:file_id",prefix).as_str(), get(process_public_read_access))
-        //upload view _KEY
-        //.route(format!("{}/direct/upload",prefix).as_str(), post(direct_upload))
-        .route(format!("{}/delete/:file_id", prefix).as_str(), delete(delete_file_using_api_key))
+        .route(format!("{}/preview/:file_id",prefix).as_str(), get(process_public_read_access))//Legal Alias
+        .route(format!("{}/v1/preview/:file_id",prefix).as_str(), get(process_public_read_access))
+        .route(format!("{}/v2/preview/:file_id",prefix).as_str(), get(process_public_read_access))//Formalization
+
+        .route(format!("{}/delete/:file_id", prefix).as_str(), delete(delete_file_using_api_key))//Legal Alias
+        .route(format!("{}/v1/delete/:file_id", prefix).as_str(), delete(delete_file_using_api_key))
+        .route(format!("{}/v2/delete/:file_id", prefix).as_str(), delete(delete_file_using_api_key))//Formalization
 		//no body limit
 		.layer(DefaultBodyLimit::disable());
     return router;
